7f2fad8964b0b01ed34cbc68dff2a5f6eb2b1867
vaibhav164 <vai1641996@gmail.com>
2024-07-19 03:45:45 +0530

    Learned to use button in Android App

--- app/src/main/java/com/example/kotlinhelloworld/MainActivity.kt
+++ app/src/main/java/com/example/kotlinhelloworld/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.kotlinhelloworld
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -10,11 +13,31 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+        setContentView(R.layout.activity_main).apply {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.LinearLayoutMain)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }}
+        val ButtonHot = findViewById<Button>(R.id.btnWarm)
+        val ButtonCool = findViewById<Button>(R.id.btnCool)
+        val ResetButton= findViewById<Button>(R.id.resetButton)
+        ResetButton.setBackgroundColor(ContextCompat.getColor(this,R.color.white))
+        ResetButton.setTextColor(ContextCompat.getColor(this,R.color.black))
+        val LinearLayOutMain = findViewById<LinearLayout>(R.id.LinearLayoutMain)
+            ButtonCool.setBackgroundColor(ContextCompat.getColor(this,R.color.blue))
+            ButtonHot.setBackgroundColor(ContextCompat.getColor(this,R.color.red))
+        ButtonHot.setOnClickListener{
+//            change background color to red or orange
+            LinearLayOutMain.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
+        }
+        ButtonCool.setOnClickListener{
+//             change background color to Blue or sky blue
+        LinearLayOutMain.setBackgroundColor(ContextCompat.getColor(this, R.color.blue))
+        }
+        ResetButton.setOnClickListener{
+            LinearLayOutMain.setBackgroundColor(ContextCompat.getColor(this,R.color.white))
+        }
     }
+
 }
